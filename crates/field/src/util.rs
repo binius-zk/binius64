a724806ad9b72b67c7b30b2c25d2aca731bf541d
Jim Posen <jim.posen@gmail.com>
2025-11-22 15:23:59 -0300

    [field] Unit test powers util function

--- crates/field/src/util.rs
+++ crates/field/src/util.rs
@@ -8,3 +8,19 @@ use crate::Field;
 pub fn powers<F: Field>(val: F) -> impl Iterator<Item = F> {
 	iter::successors(Some(F::ONE), move |&power| Some(power * val))
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{BinaryField, BinaryField128bGhash};
+
+	#[test]
+	fn test_powers_against_pow() {
+		let generator = BinaryField128bGhash::MULTIPLICATIVE_GENERATOR;
+		let power_values: Vec<_> = powers(generator).take(10).collect();
+
+		for i in 0..10 {
+			assert_eq!(power_values[i], generator.pow([i as u64]));
+		}
+	}
+}
